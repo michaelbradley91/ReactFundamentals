8fad76387219016aafd8dd9f563dbac4ef577568
Michael Bradley <michael.bradley@hotmail.co.uk>
2017-04-27 16:22:32 +0100

    Trying to get property type validation to work...

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class App extends Component {
+class App extends React.Component {
   render() {
+    let txt = this.props.txt
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    <div>
+      <h1>{txt}</h1>
+    </div>
+    )
   }
 }
 
-export default App;
+App.propTypes = {
+  cat: PropTypes.string.isRequired
+}
+
+export default App
