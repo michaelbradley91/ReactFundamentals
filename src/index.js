cbbba55bfbbc24d7d117cc0cbdd25c3b2d1035a1
Michael Bradley <michael.bradley@hotmail.co.uk>
2017-04-29 19:45:47 +0100

    Cleaning up pre TODO app

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(
-  <App txt="This is the property text" cat="3" />,
-  document.getElementById('root')
-);
